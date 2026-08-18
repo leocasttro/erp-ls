628928d6628921c29d263038653e922e9737b6c6
leocasttro <leocasttro01@icloud.com>
2026-08-18 14:58:20 -0300

    feat(metadata): integrar typeorm repository no metadata.module e app.module

--- src/metadata/infra/metadata.module.ts
+++ src/metadata/infra/metadata.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { EntityDefinitionController } from './controllers/entity-definition.controller';
 import { CreateEntityDefinitionUseCase } from '../application/use-cases/create-entity-definition.use-case';
 import { ENTITY_DEFINITION_REPOSITORY_TOKEN } from '../application/ports/entity-definition.repository';
-import { InMeoryEntityDefinitionRepository } from './persistence/in-memory-entity- definition.repository';
+import { TypeOrmEntityDefinitionRepository } from './persistence/typeorm-entity-definition.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntityDefinitionModel } from './persistence/typeorm/entity-definition.model';
+import { FieldDefinitionModel } from './persistence/typeorm/field-definition.model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EntityDefinitionModel, FieldDefinitionModel])],
   controllers: [EntityDefinitionController],
   providers: [
     CreateEntityDefinitionUseCase,
     {
       provide: ENTITY_DEFINITION_REPOSITORY_TOKEN,
-      useClass: InMeoryEntityDefinitionRepository,
+      useClass: TypeOrmEntityDefinitionRepository,
     },
   ],
 })
