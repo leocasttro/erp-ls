0c6cf164f70f4c6d719deba6b579281ff2cfa2c1
leocasttro <leocasttro01@icloud.com>
2026-08-18 13:17:13 -0300

    feat(core): adicionar estrutura base e modulo de multi-tenancy

---
+++ src/tenant/domain/entities/tenant.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity } from '@/core/domain/entities/base.entity';
+
+export class Tenant extends BaseEntity {
+  name!: string;
+  slug!: string;
+  isActive!: boolean;
+
+  constructor(partial: Partial<Tenant>) {
+    super(partial.id, partial.createdAt, partial.updatedAt);
+    Object.assign(this, partial);
+
+    this.isActive ??= true;
+  }
+
+  deactivate(): void {
+    this.isActive = false;
+    this.markAsUpdated();
+  }
+}
