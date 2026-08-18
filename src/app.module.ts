e71e1f5f93ae194f7d86520c2e1bd9eb96d5723e
leocasttro <leocasttro01@icloud.com>
2026-08-18 14:03:29 -0300

    feat(app): registrar modulos e dependencias no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MetadaModule } from './metadata/infra/metadata.module';
 
 @Module({
-  imports: [],
+  imports: [MetadaModule],
   controllers: [AppController],
   providers: [AppService],
 })
